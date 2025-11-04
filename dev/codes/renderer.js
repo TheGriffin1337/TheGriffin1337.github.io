2b11a16ce1f9d751b9ef99e824e1653e79f28969
The Griffin <wastcott7@yahoo.com>
2025-11-04 19:26:47 +0100

    Update renderer.js

--- dev/codes/renderer.js
+++ dev/codes/renderer.js
@@ -4,6 +4,10 @@ const gl = canvas.getContext("webgl");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-if(!gl){
-  alert("WebGL not supported!")
+if (!gl) {
+  alert("WebGL not supported");
 }
+
+// Clear screen
+gl.clearColor(0.53, 0.81, 0.92, 1.0); // sky color
+gl.clear(gl.COLOR_BUFFER_BIT);
